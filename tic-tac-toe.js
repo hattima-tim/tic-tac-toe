9480a5a9c0d5cb1875454e5bf497e99514856c1d
ridwan <irfanridwan20@gmail.com>
2021-04-03 21:08:30 +0600

    fixed bug and added logic for deciding a tie

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,12 @@
 let gameBoard=(()=>{
     let gameArray=[];
     let gameBoardDiv=document.querySelectorAll(".squarebox");
+    let showWinner=document.querySelector('#show_winner');
     let restartButton=document.querySelector('#restart');
     return {
         gameArray,
         gameBoardDiv,
+        showWinner,
         restartButton
     }
 })()
@@ -13,7 +15,6 @@ let players=()=>{
         gameBoard.gameArray.push(marker);
         gameBoard.gameBoardDiv[i].textContent=`${marker}`
     }
-    let point=0;
     let count={
         column1:0,
         column2:0,
@@ -24,99 +25,83 @@ let players=()=>{
         rightCross:0,
         leftCross:0
     };
-    let _clickCount=(column_row_name,play,a,b,c,marker)=>{
+    let _clickCount=(column_row_name,_a,_b,marker,check_draw)=>{
         count[column_row_name]+=1;
         if (count[column_row_name]%3===0){
-            play.check_if_winner(a,b,c,marker);
+            gameBoard.showWinner.textContent=`${marker} won`            
+        }
+        else if(gameBoard.gameArray.length===9 && check_draw==='true' && gameBoard.showWinner.textContent===''){
+            gameBoard.showWinner.textContent="Draw"
         }
     }
-    let clickCount=(i,play,marker,a,b)=>{
-        (a>b)?marker==="x":marker==="O"    
+    let clickCount=(i,marker,_a,_b)=>{
+        (_a>_b)?marker==="x":marker==="O"    
         switch(i){
             case 0:
-                _clickCount('row1',play,0,2,1,`${marker}`);
-                _clickCount('column1',play,0,6,3,`${marker}`);
-                _clickCount('rightCross',play,0,8,4,`${marker}`)
+                _clickCount('row1',0,2,`${marker}`);
+                _clickCount('column1',0,6,`${marker}`);
+                _clickCount('rightCross',0,8,`${marker}`,'true')
                 break;
             case 3:
-                _clickCount('row2',play,3,5,1,`${marker}`);
-                _clickCount('column1',play,0,6,3,`${marker}`);
+                _clickCount('row2',3,5,`${marker}`);
+                _clickCount('column1',0,6,`${marker}`,'true');
                 break;
             case 6:
-                _clickCount('row3',play,6,8,1,`${marker}`);
-                _clickCount('column1',play,0,6,3,`${marker}`);
-                _clickCount('leftCross',play,2,6,2,`${marker}`);
+                _clickCount('row3',6,8,`${marker}`);
+                _clickCount('column1',0,6,`${marker}`);
+                _clickCount('leftCross',2,6,`${marker}`,'true');
                 break;
             case 1:
-                _clickCount('row1',play,0,2,1,`${marker}`);
-                _clickCount('column2',play,1,7,3,`${marker}`)
+                _clickCount('row1',0,2,`${marker}`);
+                _clickCount('column2',1,7,`${marker}`,'true')
                 break;
             case 4:
-                _clickCount('row2',play,3,5,1,`${marker}`);
-                _clickCount('column2',play,1,7,3,`${marker}`);
-                _clickCount('rightCross',play,0,8,4,`${marker}`);
-                _clickCount('leftCross',play,2,4,6,`${marker}`);
+                _clickCount('row2',3,5,`${marker}`);
+                _clickCount('column2',1,7,`${marker}`);
+                _clickCount('rightCross',0,8,`${marker}`);
+                _clickCount('leftCross',2,4,`${marker}`,'true');
                 break;
             case 7:
-                _clickCount('row3',play,6,8,1,`${marker}`);
-                _clickCount('column2',play,1,7,3,`${marker}`);
+                _clickCount('row3',6,8,`${marker}`);
+                _clickCount('column2',1,7,`${marker}`,'true');
                 break;
             case 2:
-                _clickCount('row1',play,0,2,1,`${marker}`);
-                _clickCount('column3',play,2,8,3,`${marker}`);
-                _clickCount('leftCross',play,2,6,2,`${marker}`);
+                _clickCount('row1',0,2,`${marker}`);
+                _clickCount('column3',2,8,`${marker}`);
+                _clickCount('leftCross',2,6,`${marker}`,'true');
                 break;
             case 5:
-                _clickCount('row2',play,3,5,1,`${marker}`);
-                _clickCount('column3',play,2,8,3,`${marker}`)
+                _clickCount('row2',3,5,`${marker}`);
+                _clickCount('column3',2,8,`${marker}`,'true')
                 break;
             case 8:
-                _clickCount('row3',play,6,8,1,`${marker}`);
-                _clickCount('column3',play,2,8,3,`${marker}`);
-                _clickCount('rightCross',play,0,8,4,`${marker}`);
+                _clickCount('row3',6,8,`${marker}`);
+                _clickCount('column3',2,8,`${marker}`);
+                _clickCount('rightCross',0,8,`${marker}`,'true');
                 break;
         }   
 }
     return {
         renderContent,
-        clickCount,
-        point
+        clickCount
     }
 }
 let playerX=players();
 let playerO=players();
-let play=(()=>{
-    let check_if_winner=(a,b,c,marker)=>{
-        for (let i=a;i<=b;i+=c){
-            if (marker==='x' && gameBoard.gameBoardDiv[i].textContent===`${marker}`){
-                playerX.point+=1;
-            }
-            else if (marker==='o' && gameBoard.gameBoardDiv[i].textContent===`${marker}`){
-                playerO.point+=1;
-            }
-        }
-        if (playerX.point==3 || playerO.point==3){
-            alert(`Player ${marker} Win`)
-        }    
-    }
-    return {
-    	check_if_winner
-    }
-})()
 let displayController=(()=>{
     let a=1;
     let b=0;
     let addLisetener=()=>{
         for (let i=0;i<gameBoard.gameBoardDiv.length;i++){
             gameBoard.gameBoardDiv[i].addEventListener('click',()=>{
-                if (a>b && gameBoard.gameBoardDiv[i].textContent=="" && (playerX.point!=3 && playerO.point!=3)){
+                if (a>b && gameBoard.gameBoardDiv[i].textContent=="" && gameBoard.showWinner.textContent===""){
                     playerX.renderContent(i,"x")
-                    playerX.clickCount(i,play,"x",a,b);
+                    playerX.clickCount(i,"x",a,b);
                     b++;
                 }
-                else if (b==a && gameBoard.gameBoardDiv[i].textContent=="" && (playerX.point!=3 && playerO.point!=3)) {
+                else if (b==a && gameBoard.gameBoardDiv[i].textContent=="" && gameBoard.showWinner.textContent==="") {
                     playerO.renderContent(i,"o")
-                    playerO.clickCount(i,play,"o",a,b);
+                    playerO.clickCount(i,"o",a,b);
                     a++;
                 }
             })
@@ -129,7 +114,9 @@ let displayController=(()=>{
                 a=1;
                 b=0;
                 playerX=players();
-                playerO=players();  
+                playerO=players();
+                gameBoard.gameArray=[];
+                gameBoard.showWinner.textContent=""  
             }
         })
     }
